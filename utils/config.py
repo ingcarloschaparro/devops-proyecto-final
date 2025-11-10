8261292ef796f9d855c308444503940ab1018e90
cachaparro <cachaparro@modusoftware.com>
2025-11-09 21:10:59 -0500

    Escenario Integración Continua exitoso y de entrega continua exitoso

--- utils/config.py
+++ utils/config.py
@@ -5,7 +5,7 @@ class Config:
     TOKEN = "f18dadb0-c911-46ac-854f-e552ef323cda"
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
-        "postgresql://postgres:users12345@192.168.1.13:5432/postgres"
+        "postgresql://postgres:postgres123456@postgres.crgu42es8gyo.us-east-2.rds.amazonaws.com:5432/postgres"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     TESTING = os.getenv('TEST_ENV', 'False') == 'True'
